fe17517b90be1d00c92bea5e56a6f5884d2959ec
tranthuykieu <thuykieu2423@gmail.com>
2020-03-13 20:02:28 +0700

    edit format - pw4 - part2

--- 04.practical.work.shell.signal.c
+++ 04.practical.work.shell.signal.c
@@ -36,52 +36,52 @@ int main(int argc, char const *argv[]) {
     signal(SIGTERM, handler);
 
 
-	while (1) {
-		printf("Enter command:");
-		fgets(input, sizeof(input), stdin);
+    while (1) {
+        printf("Enter command:");
+        fgets(input, sizeof(input), stdin);
 
-		// initialization of args everytime
-		memset(args, 0, sizeof(args));
-		
-		// transform the input string
-		// to array of args
-		// so that execvp can use
-		int argc = 0;
-		int len = strlen(input);
-		char *prevArg = input;
-		for (int i = 0; i < len; i++) {
-			if (input[i] == ' ') {
-				args[argc++] = prevArg;
-				prevArg = &input[i+1];
-				input[i] = '\0';
-			}
-			if (input[i] == '\n') {
-				input[i] = '\0';
-			}
-		}
-		args[argc++] = prevArg;
+        // initialization of args everytime
+        memset(args, 0, sizeof(args));
+        
+        // transform the input string
+        // to array of args
+        // so that execvp can use
+        int argc = 0;
+        int len = strlen(input);
+        char *prevArg = input;
+        for (int i = 0; i < len; i++) {
+            if (input[i] == ' ') {
+                args[argc++] = prevArg;
+                prevArg = &input[i+1];
+                input[i] = '\0';
+            }
+            if (input[i] == '\n') {
+                input[i] = '\0';
+            }
+        }
+        args[argc++] = prevArg;
 
-		// dump the info for debugging purpose
-		printf("Input : %s\n", input);
-		if (strcmp(input, "quit") == 0) {
-			break;
-		}
+        // dump the info for debugging purpose
+        printf("Input : %s\n", input);
+        if (strcmp(input, "quit") == 0) {
+            break;
+        }
 
-		printf("- argc : %d\n", argc);
-		printf("- args : \n");
-		for (int i = 0; i <= argc; i++) {
-			printf("  + args[%d]=%s\n", i, args[i]);
-		}
+        printf("- argc : %d\n", argc);
+        printf("- args : \n");
+        for (int i = 0; i <= argc; i++) {
+            printf("  + args[%d]=%s\n", i, args[i]);
+        }
 
-		// fork() + exec() combo
-		pid = fork();
-		if (pid == 0) {
-			execvp(args[0], args);
-		}
-		else {
-			waitpid(pid, NULL, 0);
-		}
-	}
-	printf("Terminating.\n");
-	return 0;
+        // fork() + exec() combo
+        pid = fork();
+        if (pid == 0) {
+            execvp(args[0], args);
+        }
+        else {
+            waitpid(pid, NULL, 0);
+        }
+    }
+    printf("Terminating.\n");
+    return 0;
 }
